b7134d2807e87b19d44c4e2ea90855c6176e5512
Melissa Pastore <melissa.pastore@gmail.com>
2021-06-18 13:34:26 -0400

    feat: set up basic structure and components

---
+++ src/components/Pin.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Pin = () => {
+  return <div>I'm the pin</div>;
+};
+
+export default Pin;
---
+++ src/components/PinBoard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Pin from "./Pin";
+
+const PinBoard = () => {
+  return (
+    <div>
+      I'm the pin board
+      <Pin />
+    </div>
+  );
+};
+
+export default PinBoard;
